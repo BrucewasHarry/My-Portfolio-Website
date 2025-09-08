9cefcfdc4d5570bb91390e083d46df504169c59d
BrucewasHarry <harryforstyles8@gmail.com>
2025-09-08 19:03:19 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,12 @@
+// Simple JS for smooth scrolling
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+  anchor.addEventListener("click", function(e) {
+    e.preventDefault();
+    document.querySelector(this.getAttribute("href")).scrollIntoView({
+      behavior: "smooth"
+    });
+  });
+});
+
+// Console greeting
+console.log("Welcome to Dileep Veeraa's Portfolio 🚀");
